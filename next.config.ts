c02c69b186f7a035adc85dc0ef9e3194bff346c3
ishmeet124 <ishmeeeee44@email.com>
2026-09-05 14:24:07 +0530

    website changes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/aboutus",
+        destination: "/search-driven-performance-marketing-company",
+        permanent: true,
+      },
+    ];
+  },
+
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
     },
   },
+
   images: {
     qualities: [75, 90],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
     ],
   },
